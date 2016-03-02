dadc14979b0fcb73375a87def27a5c707a389d6b
amazing <grace_gc@hotmail.com>
2016-03-01 19:57:16 -0500

    added capability to remove products

--- client/product/product.component.js
+++ client/product/product.component.js
@@ -7,16 +7,17 @@ angular.module('led').directive('product', function ()
         restrict:'E',
         templateUrl:'client/product/product.html',
         controllerAs:'productCtrl',
-        controller: function ($scope,$stateParams, $meteor, $reactive){
+        controller: function ($scope, $stateParams, $meteor, $reactive, $location){
             $reactive(this).attach($scope);
-            //this.categories = $meteor.collection(Categories);
             this.helpers({
                     product: ()=> {
                     return Products.findOne({_id: $stateParams.prodId});
-        }
-        });
-
-
+                }
+            });
+            this.removeProduct = (product) => {
+                Products.remove({_id: product._id});
+                $location.path("/categories/" + $stateParams.catId);
+            }
         }
     }
 });
