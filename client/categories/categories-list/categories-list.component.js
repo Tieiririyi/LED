4126dc660bea08a3eecfb12ece51e6be5be86354
amazing <grace_gc@hotmail.com>
2016-03-01 19:57:01 -0500

    added capability to add products

--- client/categories/categories-list/categories-list.component.js
+++ client/categories/categories-list/categories-list.component.js
@@ -10,13 +10,20 @@ angular.module('led').directive('categoriesList', function ()
         controller: function ($scope,$meteor, $reactive){
             $reactive(this).attach($scope);
             //this.categories = $meteor.collection(Categories);
+
+            this.newProduct = {};
+
             this.helpers({
                 categories: ()=> {
                     return Categories.find({});
                     }
                 })
 
-
+            this.addProduct = () => {
+                this.newProduct.categoryId = Categories.findOne({"categoryName": this.newProduct.categoryName})._id;
+                Products.insert(this.newProduct);
+                this.newProduct = {};
+            };
 
             this.newCategory = {};
         }
