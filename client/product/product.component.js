1093fe29704e1d830a5665277410f92fea332172
amazing <grace_gc@hotmail.com>
2016-03-02 21:14:06 -0500

    added functionality to edit products

--- client/product/product.component.js
+++ client/product/product.component.js
@@ -9,13 +9,42 @@ angular.module('led').directive('product', function ()
         controllerAs:'productCtrl',
         controller: function ($scope, $stateParams, $meteor, $reactive, $location){
             $reactive(this).attach($scope);
+
+            this.newCategory = {};
+
             this.helpers({
                     product: ()=> {
-                    return Products.findOne({_id: $stateParams.prodId});
-                }
+                        return Products.findOne({_id: $stateParams.prodId});
+                    }
             });
+
+            this.helpers({
+                    categories: ()=> {
+                        return Categories.find({});
+                    }
+            });
+
             this.removeProduct = (product) => {
                 Products.remove({_id: product._id});
+                $location.path("/categories/" + $stateParams.catId);
+            }
+
+            this.editProduct = () => {
+
+                if (this.product.categoryName == "new"){
+                    Categories.insert({
+                        "categoryName": this.newCategory.categoryName,
+                        "categoryDescription": this.newCategory.categoryDescription
+                    });
+                    this.product.categoryId = Categories.findOne({"categoryName": this.newCategory.categoryName})._id;
+                }
+                else{
+                    this.product.categoryId = Categories.findOne({"categoryName": this.product.categoryName})._id;
+                }
+
+
+                Products.update({_id: this.product._id}, this.product);
+
                 $location.path("/categories/" + $stateParams.catId);
             }
         }
