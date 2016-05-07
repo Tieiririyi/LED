278976795dd4082812b1ba9d2480c80255c98b34
Tieiririyi <tw173@hotmail.com>
2016-05-06 23:49:01 -0400

    product list page update

--- client/products/products-list/products-list.component.js
+++ client/products/products-list/products-list.component.js
@@ -25,11 +25,41 @@ angular.module('led').directive('productsList', function ()
                         };
                     });
                 },
-                ledtypes: ()=>{
-                    return Ledtypes.find({});
+                currentCat: ()=> {
+                    console.log($stateParams.id)
+                    return Categories.findOne({_id: $stateParams.id});
+                }
+                }
+            );
+
+            $scope.selectedLedtypes =[];
+
+            this.getValuesFor = function(propName){
+                return (Products.find({categoryId: $stateParams.id, status: true}) || []).map(function (w) {
+                    return w[propName];
+                }).filter(function (w, idx, arr) {
+                    if (w == "") return false;
+                    return arr.indexOf(w) === idx;
+                });
+            }
+            this.ledtypeFilter = function (product) {
+
+                if ($scope.selectedLedtypes.length > 0) {
+                    if ($.inArray(product.info.ledtype, $scope.selectedLedtypes) < 0)
+                        return;
+                }
+
+                return product;
+            };
+
+            this.includetype = function(selectedledtype){
+                var i = $.inArray(selectedledtype, $scope.selectedLedtypes);
+                if (i > -1) {
+                    $scope.selectedLedtypes.splice(i, 1);
+                } else {
+                    $scope.selectedLedtypes.push(selectedledtype);
                 }
             }
-            );
         }
     }
 });
